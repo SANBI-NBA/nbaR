50169495c8c3239b34db824787cf9708f4760f90
TashBes <N.besseling@sanbi.org.za>
2025-10-08 16:30:01 +0200

    added scale text to map to increase legend size

--- R/maps.R
+++ R/maps.R
@@ -112,7 +112,8 @@ if(GEOM == "vector"){
 if(LEGEND == TRUE){
 
   map_leg <- map +
-    ggplot2::theme(legend.key.size = ggplot2::unit(0.8* SCALE_TEXT,"line"),
+    ggplot2::theme(legend.spacing.y = unit(0.3, 'cm')
+                   legend.key.size = ggplot2::unit(0.8* SCALE_TEXT,"line"),
                    legend.text = element_text(size = 16* SCALE_TEXT),
                    legend.position = "inside",
                    legend.justification = c("right", "bottom"),
